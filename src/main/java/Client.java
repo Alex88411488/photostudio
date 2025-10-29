ca29ef419593b9659d8c4d61f8b00cb847ebc0f5
Alexandr <sanijttt@gmail.com>
2025-10-29 10:40:03 +0300

    feat(models): add Photographer, Client, PhotoSession models

---
+++ src/main/java/Client.java
@@ -0,0 +1,25 @@
+
+
+public class Client {
+    private final int id;
+    private String name;
+    private String phone;
+
+    public Client(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getPhone() { return phone; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    @Override
+    public String toString() {
+        return String.format("Client{id=%d, name='%s', phone='%s'}", id, name, phone);
+    }
+}
